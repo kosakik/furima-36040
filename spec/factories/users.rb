b50cdffa67a5b566a1d4356b1ca07bce1ecc2f9c
kosakik <katsuo.karaage@gmail.com>
2021-10-03 19:10:07 +0900

    モデルの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-    last_name             { Gimei.name.last.kanji}
-    first_name            { Gimei.name.first.kanji}
-    last_name_kana        { Gimei.name.last.katakana}
-    first_name_kana       { Gimei.name.first.katakana}
-    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    last_name             {Gimei.name.last.kanji}
+    first_name            {Gimei.name.first.kanji}
+    last_name_kana        {Gimei.name.last.katakana}
+    first_name_kana       {Gimei.name.first.katakana}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
   end
 end
