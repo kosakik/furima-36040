d64c2acbf0cbc9096314189760fd67d11863b570
kosakik <katsuo.karaage@gmail.com>
2021-10-03 19:57:09 +0900

    コメント箇所の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,8 +79,20 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
-      it "passwordが半角英数字混合でなければ登録できない" do
+      it "passwordが半角英字のみでは登録できない" do
         @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+
+      it "passwordが半角数字のみでは登録できない" do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+
+      it "passwordが全角文字を含むパスワードでは登録できない" do
+        @user.password = 'aa00ああ'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
       end
