ef1915a3860bb7e35bbedcb71a7a5fbcc7125043
kosakik <katsuo.karaage@gmail.com>
2021-10-11 17:11:41 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :sales_status_id
+  belongs_to :shipping_fee_status_id
+  belongs_to :prefecture_id
+  belongs_to :scheduled_delivery_id
+
   belongs_to :user
   has_one_attached :image
 
