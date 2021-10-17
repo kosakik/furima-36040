31ac7a12bfa2974c0df4dbad5511d6496191292d
kosakik <katsuo.karaage@gmail.com>
2021-10-17 12:05:12 +0900

    ルーティング、コントローラーの設定(出品機能)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,24 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image,:name,:explain,:category_id,:sales_status_id,:shipping_fee_status_id,:shipping_fee_status_id,:scheduled_delivery_id,:price,).merge(user_id: current_user.id)
   end
 
 end
