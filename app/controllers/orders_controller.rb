df4b3da3542cce437aba9029407d1918a69247d2
kosakik <katsuo.karaage@gmail.com>
2021-11-14 18:26:37 +0900

    orderコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,29 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
   def index
+    @order_payment = OrderPayment.new
+  end
+
+  def create
+    @order_payment = OrderPayment.new(order_params)
+    if @order_payment.valid?
+      @order_payment.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_payment).permit(:post_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end  
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
 
